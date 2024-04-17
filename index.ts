60a32f18d1f6c96a0b6b425abb3b88e3b803d139
smartydani12345 <smartydaniazhar234@gmail.com>
2024-04-17 07:23:02 +0500

    push my word-counter to github

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+//this line is called shebang .it tells the operating system to run it with node.js
+
+
+//import the "inquirer" module , which is a command line interface for Node.js.
+import inquirer from "inquirer";
+
+// declare a constant variable `answers` and assign it to the result of the inquirer.prompt function
+
+const answers: {
+  Sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "Sentence",
+    type: "input",
+    message: "Enter your sentence to count the word: ",
+  },
+]);
+
+const words = answers.Sentence.trim().split (" ");
+
+//print the array of words in the console
+
+console.log(words);
+
+//print the word count of the sentence to the console
+
+console.log(`Your sentence word count is ${words.length}`);
